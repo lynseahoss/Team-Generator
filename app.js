3289c9efdbc13d522b740b6c44a4f013e40e78ae
Lindsey Lawson <lynseahoss@gmail.com>
2020-05-28 19:31:12 -0400

    added class to employee and manager- but most likely will change manager info

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const teamMember = []
 
 
 
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
