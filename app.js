b9666d235ba588eec921ef7c554e1d99b6d7cf8a
Lindsey Lawson <lynseahoss@gmail.com>
2020-05-30 15:12:22 -0400

    used semanitic ui for templates

--- app.js
+++ app.js
@@ -10,102 +10,144 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamArray = [];
 
-async function init(){
-    try{
-        //array for employees
-        const emp = []
-        const empQuests = [name, id, email, officeNumber, github, school]
-    }
-}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-function managerQuestions(){
-   return inquirer.prommpt([
-       {
-           name: "name",
-           type: "input",
-           message: "Enter the Manager's name"
-       },
-       {
+const renderList = () =>{
+    //if the output dir doesn't exist then use mkdirsync to create output dir
+    if(!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamArray), )
+}
+
+const teamMembs = () => {
+  inquirer
+    .prompt([
+      {
+        name: "nextEmployee",
+        type: "list",
+        message: "Choose the type of employee",
+        choices: ["Engineer", "Intern", "Exit List"]
+      }
+    ])
+    .then(answers => {
+      switch (answers.nextEmployee) {
+        case "Engineer":
+          engineerQuestions();
+          break;
+        case "Intern":
+          internQuestions();
+          break;
+        default:
+            renderList()
+          break;
+      }
+    });
+};
+//named arrow function
+const managerQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Enter the Manager's name"
+      },
+      {
         name: "id",
         type: "input",
         message: "Enter the Manager's ID"
-    },
-    {
+      },
+      {
         name: "email",
         type: "input",
         message: "Enter the Manager's email"
-    },
-    {
+      },
+      {
         name: "officeNum",
         type: "input",
         message: "Enter the Manager's office number"
+      }
+    ])
+    //deconstruct
+    .then(({ name, id, email, officeNum }) => {
+      const newManager = new Manager(name, id, email, officeNum);
+      teamArray.push(newManager);
+      teamMembs();
+    });
+};
+
+function engineerQuestions() {
+  inquirer.prompt([
+    {
+      name: "name",
+      type: "input",
+      message: "Enter the Engineer's name"
     },
-   
-   ])
-
- 
+    {
+      name: "id",
+      type: "input",
+      message: "Enter the Engineer's ID"
+    },
+    {
+      name: "email",
+      type: "input",
+      message: "Enter the Engineer's email"
+    },
+    {
+      name: "github",
+      type: "input",
+      message: "Enter the Engineer's github"
+    }
+  ])
+  .then(({ name, id, email, github}) => {
+    const newEngineer = new Engineer(name, id, email, github);
+    teamArray.push(newEngineer);
+    teamMembs();
+  });
 }
 
-function engineerQuestions(){
-    return inquirer.prompt([
-        {
-            name: "name",
-            type: "input",
-            message: "Enter the Engineer's name"
-        },
-        {
-         name: "id",
-         type: "input",
-         message: "Enter the Engineer's ID"
-     },
-     {
-         name: "email",
-         type: "input",
-         message: "Enter the Engineer's email"
-     },
-     {
-            name: "github",
-            type: "input",
-            message: "Enter the Engineer's github"
-        }
-
-    ])
+function internQuestions() {
+  inquirer.prompt([
+    {
+      name: "name",
+      type: "input",
+      message: "Enter the Itern's name"
+    },
+    {
+      name: "id",
+      type: "input",
+      message: "Enter the Intern's ID"
+    },
+    {
+      name: "email",
+      type: "input",
+      message: "Enter the Intern's email"
+    },
+    {
+      name: "school",
+      type: "input",
+      message: "Enter the Intern's school"
+    }
+  ])
+  .then(({ name, id, email, school }) => {
+    const newIntern = new Intern(name, id, email, school);
+    teamArray.push(newIntern);
+    teamMembs();
+  });
 }
 
-function internQuestions(){
-    return inquirer.prompt([
-        {
-            name: "name",
-            type: "input",
-            message: "Enter the Itern's name"
-        },
-        {
-         name: "id",
-         type: "input",
-         message: "Enter the Intern's ID"
-     },
-     {
-         name: "email",
-         type: "input",
-         message: "Enter the Intern's email"
-     },
-     {
-            name: "github",
-            type: "input",
-            message: "Enter the Intern's school"
-        }
-
-    ])
-}
+managerQuestions()
 
 
 
 
 
-
+/////////////////////////////////////////////////////////////////////////////////////
+///Directions
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
